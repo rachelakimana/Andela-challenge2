b4de4de2d968f508aa5996ba88341e33580f0dbf
rachelakimana <rachelakimana@gmail.com>
2021-08-16 18:29:11 +0200

    new both desktop and mobile design complete without validation

---
+++ main.js
@@ -0,0 +1,12 @@
+function validateForm(x) {
+    let x = document.getElementsByClassName('mymessage').innerHTML = "Please provide a valid e-mail address";
+        console.log('>>>>>>>>>>')
+      return x;
+    
+  }
+
+     
+  function hideParagraph(x) {
+    let x = document.getElementsByClassName('mymessage');
+    x.style.visibility = 'hidden';
+}
