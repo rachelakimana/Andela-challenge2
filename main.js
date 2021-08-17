db8b492be58bcc6f7f89845d0c9d8fe83c46e15a
rachelakimana <rachelakimana@gmail.com>
2021-08-17 20:13:48 +0200

    both desktop and mobile design complete with some validation

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-function validateForm(x) {
-    let x = document.getElementsByClassName('mymessage').innerHTML = "Please provide a valid e-mail address";
-        console.log('>>>>>>>>>>')
-      return x;
+function validateForm() {
+   let  x = document.getElementById("mymessage").innerHTML = "Please provide a valid e-mail address"
+        console.log('>>>>>>>>>>'+ x)
+  document.getElementById("email").style.borderColor = "hsl(354, 100%, 66%)"
+  
     
   }
 
-     
-  function hideParagraph(x) {
-    let x = document.getElementsByClassName('mymessage');
-    x.style.visibility = 'hidden';
+  function hidParagraph() {
+   let display =  document.getElementById("mymessage").style.display="none";
+   document.getElementById("email").style.borderColor = "hsl(0, 0%, 59%)";
+    
+}
+  function ValidateEmail(input) {
+
+  var validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  if (input.value.match(validRegex)) {
+
+    alert("Your email submited successfully!");
+
+    document.form1.text1.focus();
+
+    return true;
+
+  } else {
+    document.form1.text1.focus();
+    return validateForm();
+    
+    
+
+  }
+
 }
